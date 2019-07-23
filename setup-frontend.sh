d8b1e5483904c2f6af0794dfb1b9705c814a7113
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-23 15:53:42 +0100

    typo

--- setup-frontend.sh
+++ setup-frontend.sh
@@ -3,7 +3,7 @@
 //docker build -t docker.io/jpeckover/pool_frontend:latest .
 docker pull jpeckover/pool_frontend:latest .
 
-docker run -d --name frontend --network pool_app_network -p 80:80 docker.io/jpeckover/pool_frontend:latest
+docker run -d --name frontend --network pool_app_network -p 80:80 jpeckover/pool_frontend:latest
 
 #docker ps
 #docker logs -f frontend
