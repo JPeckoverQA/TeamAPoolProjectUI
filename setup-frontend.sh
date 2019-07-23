b3f8539a55617da8089780b35b51e4590ae74ecd
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-23 15:51:07 +0100

    change build to pull from image on repo

--- setup-frontend.sh
+++ setup-frontend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker build -t docker.io/jpeckover/pool_frontend:latest .
+//docker build -t docker.io/jpeckover/pool_frontend:latest .
+docker pull jpeckover/pool_frontend:latest .
 
 docker run -d --name frontend --network pool_app_network -p 80:80 docker.io/jpeckover/pool_frontend:latest
 
