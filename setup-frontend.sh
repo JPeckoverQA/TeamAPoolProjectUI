3ff302a11968fa4af183d70b4d7c0e61634f33f9
JohnnyQA <Jonathan.Peckover@academytrainee.com>
2019-07-25 15:44:16 +0100

    remove network

--- setup-frontend.sh
+++ setup-frontend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-//docker build -t docker.io/jpeckover/pool_frontend:latest .
+#docker build -t docker.io/jpeckover/pool_frontend:latest .
 sudo docker pull jpeckover/pool_frontend:latest .
 
-sudo docker run -d --name frontend --network pool_app_network -p 80:80 docker.io/jpeckover/pool_frontend:latest
+sudo docker run -d --name frontend -p 80:80 docker.io/jpeckover/pool_frontend:latest
 
 #docker ps
 #docker logs -f frontend
