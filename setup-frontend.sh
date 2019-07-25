5e69e7c263967f4d1c50315262ea07a7f972013a
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-25 13:59:37 +0100

    remove .

--- setup-frontend.sh
+++ setup-frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #docker build -t docker.io/jpeckover/pool_frontend:latest .
-sudo docker pull jpeckover/pool_frontend:latest .
+sudo docker pull jpeckover/pool_frontend:latest
 
 sudo docker run -d --name frontend --network pool_app_network -p 80:80 docker.io/jpeckover/pool_frontend:latest
 
